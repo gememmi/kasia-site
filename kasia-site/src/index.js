0cd60c5eee2764e7ca156678664b39741125b82e
Emily <emelizadaniels@gmail.com>
2024-03-19 17:27:41 -0400

    messing around with BR and i think something broke

--- kasia-site/src/index.js
+++ kasia-site/src/index.js
@@ -1,57 +1,19 @@
+
+import ReactDOM from 'react-dom';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Root from './routes/Root';
-import ErrorPage from "./error-page";
-import Home from './routes/Home';
-import About from './routes/About';
-import Contact from './routes/Contact';
-import Gallery from './routes/Gallery';
-import Menus from './routes/Menus';
 import './index.css';
-import {
-  // BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter,} from "react-router-dom";
 
-const router = createBrowserRouter([
-  { 
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [ 
-      {index: true, element: <Home />},
-      {
-        path: "home",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        path: "gallery",
-        element: <Gallery />,
-      },
-      {
-        path: "menus",
-        element: <Menus />,
-      },
-    ]
-  }
-
+ReactDOM.render(
   
-])
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
   <React.StrictMode>
-    <RouterProvider router={ router } />
-  </React.StrictMode>
+    <BrowserRouter>
+    <Root />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- kasia-site/src/routes/Root.js
+++ kasia-site/src/routes/Root.js
@@ -1,51 +1,49 @@
+import React from 'react';
 import logo from "/Users/emilydaniels/Development/code/post-grad/kasia-site/src/images/logo-white.png";
-import { Outlet, Link, NavLink, useNavigation } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
+import ErrorPage from "/Users/emilydaniels/Development/code/post-grad/kasia-site/src/error-page.js";
+import About from "./About";
+import Gallery from "./Gallery";
+import Menus from "./Menus";
+import Contact from "./Contact";
 
 function Root() {
-  const navigation = useNavigation();
+  
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section){
+      section.scrollIntoView({ behavior: 'smooth'});
+    }
+  };
 
   return (
     <div id="topbar">
-      <Link to= {"home"}>
+      <Link onClick={() => scrollToSection('home')} to= {"home"}>
         <img src={logo} alt="dinner play and knife logo with K L" />
       </Link>
 
       <nav>
         <NavLink>
-          <Link to={"about"}>ABOUT</Link>
+          <Link onClick={() => scrollToSection('about')} to={"about"}>ABOUT</Link>
         </NavLink>
         <NavLink>
-          <Link to={"gallery"}>GALLERY</Link>
+          <Link onClick={() => scrollToSection('gallery')} to={"gallery"}>GALLERY</Link>
         </NavLink>
         <NavLink>
-          <Link to={"menus"}>MENUS</Link>
+          <Link onClick={() => scrollToSection('menus')}  to={"menus"}>MENUS</Link>
         </NavLink>
         <NavLink>
-          <Link to={"contact"}>CONTACT</Link>
+          <Link onClick={() => scrollToSection('contact')}  to={"contact"}>CONTACT</Link>
         </NavLink>
       </nav>
-      <div
-        id="outlet"
-        className={navigation.state === "loading" ? "loading" : ""}
-      >
-        <Outlet />
-      </div>
-
-      {/* <a href="/landing"><img src= {logo} alt="dinner plate and knife logo with K L"></img></a>
-      <nav>
-      <button>
-        <a href="/contact">contact</a>
-        </button>
-        <button>
-        <a href="/about">about</a>
-        </button>
-        <button>
-        <a href="/gallery">gallery</a>
-        </button>
-        <button>
-        <a href="/menus">menus</a>
-        </button>
-      </nav> */}
+      <Routes>
+          <Route path="/about" component={About} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/menus" component={Menus} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/error" component={ErrorPage} />
+        </Routes>
     </div>
   );
 }
